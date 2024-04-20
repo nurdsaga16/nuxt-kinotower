01f17130c5ddd689b06a9398a116b78c75b84dbd
nurdsaga16 <nurd.saga14@gmail.com>
2024-04-20 13:46:07 +0500

    Pagination and loading spinner were added

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,32 @@
 <script lang="ts" setup>
-  const storedFilms = useFilmsStore();
-  const storedCategories = useCategoriesStore();
-  const storedCountries = useCountriesStore();
 
-  const category = ref(null);
-  watch(category, (newCategory) => {
-    storedFilms.addCategoriesToParams(newCategory);
-  });
+const storedFilms = useFilmsStore();
+const storedCategories = useCategoriesStore();
+const storedCountries = useCountriesStore();
 
-  const country = ref(null);
-  watch(country, (newCountry) => {
-    storedFilms.addCountriesToParams(newCountry);
-  });
 
-  const sortBy = ref('name');
-  watch(sortBy, (newSortBy) => {
-    storedFilms.addSortToParams(newSortBy);
-  })
+const category = ref(null);
+watch(category, (newCategory) => {
+  storedFilms.addCategoriesToParams(newCategory);
+});
+const country = ref(null);
+watch(country, (newCountry) => {
+  storedFilms.addCountriesToParams(newCountry);
+});
+const sortBy = ref('name');
+watch(sortBy, (newSortBy) => {
+  storedFilms.addSortToParams(newSortBy);
+})
+const resetParams = () => {
+  category.value = null;
+  country.value = null;
+  sortBy.value = 'name';
 
-  const resetParams = () => {
-    category.value = null;
-    country.value = null;
-    sortBy.value = 'name';
-
-    storedFilms.fetchFilms();
-  }
 
   storedFilms.fetchFilms();
+}
+
+storedFilms.fetchFilms();
 </script>
 
 <template>
@@ -34,20 +34,16 @@
     <div class="col-md-4">
       <select class="form-select" v-model="category">
         <option selected :value="null">Все жанры</option>
-        <option v-for="category in storedCategories.categories" 
-                :key="category.id" 
-                :value="category.id">
-                {{ category.name }} ({{ category.filmCount }})
+        <option v-for="category in storedCategories.categories" :key="category.id" :value="category.id">
+          {{ category.name }} ({{ category.filmCount }})
         </option>
       </select>
     </div>
     <div class="col-md-4">
       <select class="form-select" v-model="country">
         <option selected :value="null">Все фильмы</option>
-        <option v-for="country in storedCountries.countries" 
-                :key="country.id" 
-                :value="country.id">
-                {{ country.name }}
+        <option v-for="country in storedCountries.countries" :key="country.id" :value="country.id">
+          {{ country.name }}
         </option>
       </select>
     </div>
@@ -60,61 +56,68 @@
     </div>
     <div class="col-md-1">
       <a class="text-decoration-none">
-        <button class="btn" @click="resetParams">Сбросить</button>
+        <button class="btn" @click="resetParams">Reset</button>
       </a>
     </div>
   </div>
 
-  <div class="row row-cols-1 row-cols-md-3 g-4">
-    <div class="col" v-for="film in storedFilms.films" :key="film.id">
-      <div class="card h-100">
-        <img :src="film.link_img"
-          class="card-img-top" alt="..." width="70" height="600">
-        <div class="card-body">
-          <h5 class="card-title">{{ film.name }}</h5>
-          <p class="card-text">Рейтинг: {{ film.ratingAvg }}</p>
-          <p class="card-text">Продолжительность: {{ film.duration }} мин.</p>
-          <p class="card-text" v-if="film.categories.length != 0">Жанры: <template v-for="(genre, index) in film.categories" :key="genre.id">
-            {{ (index != film.categories.length-1) ? genre.name+', ' : genre.name}}
-          </template></p>
-          <p class="card-text" v-else>Нет жанров</p>
-        </div>
-        <div class="card-footer text-center">
-          <small class="text-body-secondary">
-            <a class="text-decoration-none">
-              <button class="btn">Смотреть</button>
-            </a>
-          </small>
+  <div class="d-flex justify-content-center" v-if="storedFilms.isLoading">
+  <div class="spinner-border" style="width: 3rem; height: 3rem;" role="status">
+    <span class="visually-hidden">Loading...</span>
+  </div>
+</div>
+  <template v-else>
+    <div class="row row-cols-1 row-cols-md-4 g-4">
+      <div class="col" v-for="film in storedFilms.films" :key="film.id">
+        <div class="card h-100">
+          <img :src="film.link_img" class="card-img-top" alt="..." width="70" height="450">
+          <div class="card-body">
+            <h5 class="card-title">{{ film.name }}</h5>
+            <p class="card-text">Рейтинг: {{ film.ratingAvg }}</p>
+            <p class="card-text">Продолжительность: {{ film.duration }} мин.</p>
+            <p class="card-text" v-if="film.categories.length != 0">Жанры: <template
+                v-for="(genre, index) in film.categories" :key="genre.id">
+                {{ (index != film.categories.length - 1) ? genre.name + ', ' : genre.name }}
+              </template></p>
+            <p class="card-text" v-else>Нет жанров</p>
+          </div>
+          <div class="card-footer text-center">
+            <small class="text-body-secondary">
+              <a class="text-decoration-none">
+                <button class="btn">Смотреть</button>
+              </a>
+            </small>
+          </div>
         </div>
       </div>
     </div>
-  </div>
 
-  <!-- Pagination list films -->
+    <!-- Pagination list films -->
 
-  <nav aria-label="Page navigation example" class="my-4 d-flex justify-content-center">
-  <ul class="pagination">
-    <li class="page-item">
-      <a class="page-link" href="#" aria-label="Previous">
-        <span aria-hidden="true">&laquo;</span>
-      </a>
-    </li>
-    <li class="page-item"><a class="page-link" href="#">1</a></li>
-    <li class="page-item"><a class="page-link" href="#">2</a></li>
-    <li class="page-item"><a class="page-link" href="#">3</a></li>
-    <li class="page-item">
-      <a class="page-link" href="#" aria-label="Next">
-        <span aria-hidden="true">&raquo;</span>
-      </a>
-    </li>
-  </ul>
-</nav>
-
+    <nav aria-label="Page navigation example" class="my-4 d-flex justify-content-center">
+      <ul class="pagination">
+        <li class="page-item">
+          <a class="page-link" :class="{ 'disabled': storedFilms.page - 1 == 0 }" href="#" aria-label="Previous"
+            @click.prevent="storedFilms.changePage(storedFilms.page - 1)">
+            <span aria-hidden="true">&laquo;</span>
+          </a>
+        </li>
+        <li class="page-item" v-for="page in Math.ceil(storedFilms.total / storedFilms.size)" :key="page">
+          <a class="page-link" :class="{ 'active': page == storedFilms.page }" href="#"
+            @click.prevent="storedFilms.changePage(page)">
+            {{ page }}
+          </a>
+        </li>
+        <li class="page-item">
+          <a class="page-link"
+            :class="{ 'disabled': storedFilms.page + 1 > Math.ceil(storedFilms.total / storedFilms.size) }" href="#"
+            aria-label="Next" @click.prevent="storedFilms.changePage(storedFilms.page + 1)">
+            <span aria-hidden="true">&raquo;</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </template>
 </template>
 
-<script lang="ts" setup>
-
-</script>
-
-<style>
-</style>
+<style></style>
--- stores/useFilmsStore.ts
+++ stores/useFilmsStore.ts
@@ -3,25 +3,37 @@ import { api } from '~/api';
 
 export const useFilmsStore = defineStore('films', () => {
   const films = ref([]);
+  const page = ref(1);
+  const total = ref(0);
+  const size = 4;
+  const isLoading = ref(true);
   const params: {
     category:number|null,
     country:number|null,
     sortBy:string,
     sortDir:string,
+    size:number,
+    page:number
   } = {
     category: null,
     country: null,
     sortBy: 'name',
-    sortDir: 'desc'
+    sortDir: 'desc',
+    page: 1,
+    size
   };
 
   function addCategoriesToParams(category:number|null) {
       params.category = category;
+      page.value = 1;
+      params.page = 1;
       fetchFilms();
   }
 
   function addCountriesToParams(country:number|null) {
     params.country = country;
+    page.value = 1;
+    params.page = 1;
     fetchFilms();
   }
 
@@ -30,9 +42,17 @@ export const useFilmsStore = defineStore('films', () => {
     fetchFilms();
   }
 
+  function changePage(pages:number) {
+    page.value = pages;
+    params.page = pages;
+    fetchFilms();
+  }
+
   async function fetchFilms() {
     const response = await api.get('/films', {params});
+    isLoading.value = false;
     films.value = response.data.films;
+    total.value = response.data.total;
   }
 
   return {
@@ -40,6 +60,11 @@ export const useFilmsStore = defineStore('films', () => {
     fetchFilms,
     addCategoriesToParams,
     addCountriesToParams,
-    addSortToParams
+    addSortToParams,
+    total,
+    size,
+    page,
+    changePage,
+    isLoading
   }
 })
